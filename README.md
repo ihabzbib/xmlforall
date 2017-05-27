b5d28d0f5ea904929a576275dcab6a666cd1ffee
ihab <ihab.zbib@gmail.com>
2017-05-26 19:05:26 -0700

    Updated test file

--- README.md
+++ README.md
@@ -44,19 +44,18 @@ xmlforall.parse('menu.xml', function (err, doc) {
 ```js
 var xmlforall = require('xmlforall');
 xmlforall.parse('menu.xml', function (err, doc) {
-    var i, element, name, price;
+    var items, name, price;
 
-    for (i = 0; i < 5; i++) {
-        element = doc.getElementById(i.toString());
+    items = doc.getElementsByTagName('food');
+    items.forEach(function (element) {
         name = element.getElementsByTagName('name')[0];
         price = element.getElementsByTagName('price')[0];
-
         console.log(
             name.text + ':',
             price.text,
             '(' + price.getAttribute('currency') + ')'
         );
-    }
+    });
 });
 ```
 # menu.xml
--- test/test.js
+++ test/test.js
@@ -1,22 +1,21 @@
 'use strict';
 var xmlforall = require('../lib/parser');
 
-xmlforall.parse(__dirname + '/breakfast_menu.xml', function (err, doc) {
-    var i, element, name, price;
+xmlforall.parse(__dirname + '/menu.xml', function (err, doc) {
+    var items, name, price;
 
     if (err) {
         return console.error(err);
     }
 
-    for (i = 0; i < 5; i++) {
-        element = doc.getElementById(i.toString());
+    items = doc.getElementsByTagName('food');
+    items.forEach(function (element) {
         name = element.getElementsByTagName('name')[0];
         price = element.getElementsByTagName('price')[0];
-
         console.log(
             name.text + ':',
             price.text,
             '(' + price.getAttribute('currency') + ')'
         );
-    }
+    });
 });
